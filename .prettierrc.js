b014deabe2604d90dcc3cdb4f1b7a5739c8de0bf
RyuWoong <wjdfbd1121@gmail.com>
2024-07-10 09:07:46 +0900

    feat: First Commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// prettier
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  singleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  arrowParens: "always",
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+};
